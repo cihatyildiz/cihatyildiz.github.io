16c6f0c9e1fd70c357ba522dc82689b2f44fe6df
Cihat Yildiz <cihat.yildiz@gmail.com>
2021-07-01 23:44:15 -0700

    test

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210626.0545';
+const cacheName = 'chirpy-20210702.1443';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
